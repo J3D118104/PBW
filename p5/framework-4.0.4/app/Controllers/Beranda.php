223b6be421571d5c7cc39464be11e01510b79728
mazhara104 <muhammad.azhar1234.maa@gmail.com>
2020-10-24 21:37:52 +0700

    praktikum

---
+++ p5/framework-4.0.4/app/Controllers/Beranda.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Controllers;
+
+
+class Beranda extends BaseController{
+
+    public function index(){
+        return view(
+            'beranda_v'
+        );
+    }
+
+}
+
+?>
